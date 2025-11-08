446d886612cf551ea181d7f3698a2a2c743b96e5
abcdefghijklqwq <butrahandisya@gmail.com>
2025-11-08 19:09:47 +0700

    feat: implement Discord command registration and add ping command

---
+++ discord-commands/ping.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import type { executeCommand } from '../discord-helper/types';
+
+export const register = new SlashCommandBuilder()
+  .setName('ping')
+  .setDescription("pong's you back! (bot check)");
+
+export const execute: executeCommand = async (interaction) => {
+  return {
+    type: 4,
+    data: {
+      content: `pong! ${interaction.member?.user.username}`
+    }
+  };
+};
---
+++ discord-helper/types.ts
@@ -0,0 +1,8 @@
+import type {
+  APIApplicationCommandInteraction,
+  APIInteractionResponse
+} from 'discord.js';
+
+export type executeCommand = (
+  interaction: APIApplicationCommandInteraction
+) => Promise<APIInteractionResponse>;
