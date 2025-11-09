3bf4f6f01f3730e6b3350a7456f20d026b107875
abcdefghijklqwq <butrahandisya@gmail.com>
2025-11-09 15:03:30 +0700

    feat: add score command for submitting Nightmare Gateway scores

--- src/discord-helper/get-commands.ts
+++ src/discord-helper/get-commands.ts
@@ -2,15 +2,20 @@ import type { executeCommand } from './types';
 
 import * as pingCommand from '@/discord-commands/ping';
 import * as registerCommand from '@/discord-commands/register';
+import * as scoreCommand from '@/discord-commands/score';
 
 type CommandModule = {
-  register: typeof pingCommand.register | typeof registerCommand.register;
+  register:
+    | typeof pingCommand.register
+    | typeof registerCommand.register
+    | typeof scoreCommand.register;
   execute: executeCommand;
 };
 
 const commandModules: Record<string, CommandModule> = {
   ping: pingCommand,
-  register: registerCommand
+  register: registerCommand,
+  score: scoreCommand
 };
 
 const getCommands = () => {
