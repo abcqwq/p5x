88464353b6d3e750d561d6ef67873be7cc57cd43
abcdefghijklqwq <butrahandisya@gmail.com>
2025-12-02 20:48:27 +0700

    feat: integrate fetchUsersWithoutScores to include users without scores in the Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { PeriodProvider } from '@/react-things/providers/PeriodProvider';
 import { CompaniosProvider } from '@/react-things/providers/CompaniosProvider';
 import { MinimumScoresProvider } from '@/react-things/providers/MinimumScoresProvider';
 import { fetchActiveNightmareGatewayPeriod } from '@/handlers/fetch-nightmare-period';
+import { fetchUsersWithoutScores } from '@/handlers/fetch-users-without-scores';
 import { fetchCompanios } from '@/handlers/fetch-companios';
 import { fetchNightmareGatewayScores } from '@/handlers/fetch-gateway-scores';
 import { fetchMinimumScores } from '@/handlers/fetch-minimum-scores';
@@ -22,10 +23,20 @@ const Page = async () => {
     redirect('/nightmare-gateway-periods');
   }
 
-  const [scores, minimumScores] = await Promise.all([
+  const [scores, noScoreUsers, minimumScores] = await Promise.all([
     fetchNightmareGatewayScores(activePeriod.id),
+    fetchUsersWithoutScores(activePeriod.id),
     fetchMinimumScores(activePeriod.id)
   ]);
+
+  noScoreUsers.forEach((user) => {
+    scores?.push({
+      id: `no-score-${user.id}`,
+      first_half_score: 0,
+      second_half_score: 0,
+      user: user
+    });
+  });
 
   return (
     <PeriodProvider data={activePeriod}>
