5ef03681898724b91bd487720061fcfc6eef6d1f
abcdefghijklqwq <butrahandisya@gmail.com>
2025-12-12 21:25:40 +0700

    chore: fix og route

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,13 +1,9 @@
 import { ImageResponse } from 'next/og';
 import Main from '@/app/og/Main';
 
-export const size = {
-  width: 1200,
-  height: 630
-};
-
-export const contentType = 'image/png';
-
 export async function GET() {
-  return new ImageResponse(<Main />, size);
+  return new ImageResponse(<Main />, {
+    width: 1200,
+    height: 630
+  });
 }
